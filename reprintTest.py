00be8e5b9291b22fff0ab03a418667dc380ad2e2
ekrauser <56862932+ekrauser@users.noreply.github.com>
2023-06-21 16:49:25 -0500

    Update reprintTest.py

--- reprintTest.py
+++ reprintTest.py
@@ -1,3 +1,5 @@
+#created by ekrauser@ for UIL2 and whoever else can get some use out of it
+
 import csv
 import pyqrcode
 import win32print
@@ -7,6 +9,71 @@ import os
 
 def convert_mac_format(mac):
     return ":".join(mac[i:i+2] for i in range(0, 12, 2)).upper()
+
+def create_and_print_qr(label_text, output_directory, image_width, image_height, qr_size, text_font_size):
+    # Generate QR code
+    qr = pyqrcode.create(label_text)
+
+    # Create a safe filename by replacing colons with underscores
+    safe_label_text = label_text.replace(":", "_")
+
+    # Save QR code image
+    qr_filename = f"qr_{safe_label_text}.png"
+    qr_path = output_directory + qr_filename
+    qr.png(qr_path, scale=10)
+
+    # Create an image with the QR code and label text
+    img = Image.new("RGB", (image_width, image_height), "white")
+
+    qr_img = Image.open(qr_path)
+    qr_img = qr_img.resize((qr_size, qr_size))
+    qr_position = ((image_width - qr_size) // 2, 5)
+    img.paste(qr_img, qr_position)
+
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", text_font_size)
+    text = label_text
+    text_width, text_height = draw.textsize(text, font=font)
+    text_position = ((image_width - text_width) // 2, qr_position[1] + qr_size + 5)
+    draw.text(text_position, text, font=font, fill="black")
+
+    # Save the final image with the QR code and label text as the file name
+    final_filename = f"qr_with_text_{safe_label_text}.png"
+    final_path = output_directory + final_filename
+    img.save(final_path)
+
+    # Print image using Windows DEFAULT printer
+    print_image(img)
+
+
+def print_image(img):
+    HORZRES = 8
+    VERTRES = 10
+
+    printer_name = win32print.GetDefaultPrinter()
+
+    hdc = win32ui.CreateDC()
+    hdc.CreatePrinterDC(printer_name)
+
+    printer_name = win32print.GetPrinter(win32print.OpenPrinter(printer_name), 2)["pPrinterName"]
+    job = win32print.StartDoc(hdc.GetHandleOutput(), (printer_name, None, "RAW", 0))
+
+    win32print.StartPage(hdc.GetHandleOutput())
+
+    ratios = [1.0 * img.size[i] / hdc.GetDeviceCaps(j) for i, j in enumerate((HORZRES, VERTRES))]
+    ratio = min(ratios)
+
+    img = img.resize([int(ratio * i) for i in img.size], Image.ANTIALIAS)
+
+    dib = ImageWin.Dib(img)
+
+    r = [0, 0, hdc.GetDeviceCaps(HORZRES), hdc.GetDeviceCaps(VERTRES)]
+    dib.draw(hdc.GetHandleOutput(), r)
+
+    win32print.EndPage(hdc.GetHandleOutput())
+    win32print.EndDoc(hdc.GetHandleOutput())
+
+    hdc.DeleteDC()
 
 # Directory to save the generated QR codes
 output_directory = "qr_codes/"
@@ -26,9 +93,6 @@ if not os.path.exists(csv_file):
         writer = csv.writer(f)
         writer.writerow(["Name", "MAC Address", "Serial Number"])
 
-# Ask user if they want to reprint the name label
-reprint = input("Do you want to reprint the Name Label? (y/n): ")
-
 while True:
     # Read name and MAC addresses from the terminal
     name = input("Scan Printer Name - This is the existing QR code (or 'q' to quit): ")
@@ -39,76 +103,18 @@ while True:
 
     mac_addresses = input("Scan Bluetooth MAC addresses: ").split(",")
 
-    # If the user chose to reprint the name label
-    if reprint.lower() == 'y':
-        mac_addresses.append(name)
-
     # Iterate through each MAC address
     for mac_address in mac_addresses:
         mac_address = mac_address.strip()
 
-        if mac_address != name:
-            converted_mac = convert_mac_format(mac_address)
-        else:
-            converted_mac = mac_address
+        converted_mac = convert_mac_format(mac_address)
 
-        # Generate QR code
-        qr = pyqrcode.create(converted_mac)
+        # Create and print QR code for MAC
+        create_and_print_qr(converted_mac, output_directory, image_width, image_height, qr_size, text_font_size)
 
-        # Save QR code image
-        qr_filename = f"qr_{mac_address}.png"
-        qr_path = output_directory + qr_filename
-        qr.png(qr_path, scale=10)
-
-        qr_img = Image.open(qr_path)
-        qr_img = qr_img.resize((qr_size, qr_size))
-        qr_position = ((image_width - qr_size) // 2, 5)
-
-        # Create an image with the QR code and converted MAC address
-        img = Image.new("RGB", (image_width, image_height), "white")
-        img.paste(qr_img, qr_position)
-
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", text_font_size)
-        text = converted_mac if mac_address != name else mac_address
-        text_width, text_height = draw.textsize(text, font=font)
-        text_position = ((image_width - text_width) // 2, qr_position[1] + qr_size + 5)
-        draw.text(text_position, text, font=font, fill="black")
-
-        # Save the final image with the QR code and original MAC address as the file name
-        final_filename = f"qr_with_mac_{mac_address}.png"
-        final_path = output_directory + final_filename
-        img.save(final_path)
-
-        # Print image using Windows DEFAULT printer, so make sure you have it config'd ahead of time
-        HORZRES = 8
-        VERTRES = 10
-
-        printer_name = win32print.GetDefaultPrinter()
-
-        hdc = win32ui.CreateDC()
-        hdc.CreatePrinterDC(printer_name)
-
-        printer_name = win32print.GetPrinter(win32print.OpenPrinter(printer_name), 2)["pPrinterName"]
-        job = win32print.StartDoc(hdc.GetHandleOutput(), (printer_name, None, "RAW", 0))
-
-        win32print.StartPage(hdc.GetHandleOutput())
-
-        ratios = [1.0 * img.size[i] / hdc.GetDeviceCaps(j) for i, j in enumerate((HORZRES, VERTRES))]
-        ratio = min(ratios)
-
-        img = img.resize([int(ratio * i) for i in img.size], Image.ANTIALIAS)
-
-        dib = ImageWin.Dib(img)
-
-        r = [0, 0, hdc.GetDeviceCaps(HORZRES), hdc.GetDeviceCaps(VERTRES)]
-        dib.draw(hdc.GetHandleOutput(), r)
-
-        win32print.EndPage(hdc.GetHandleOutput())
-        win32print.EndDoc(hdc.GetHandleOutput())
-
-        hdc.DeleteDC()
-
+        # Create and print QR code for name
+        create_and_print_qr(name.strip(), output_directory, image_width, image_height, qr_size, text_font_size)
+        
         # Append name, converted MAC address, and serial number to the CSV file
         with open(csv_file, "a", newline="") as f:
             writer = csv.writer(f)
